011aa665f9aee1deed43389c54d4755c7318d2c1
Clement Noel <noelclement@yahoo.fr>
2024-12-23 16:42:24 +0100

    commit 4

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -1,18 +1,16 @@
-
 /*
 
-Question: What are the most in-demand skills for data analysts?
+Answer: What are the top skills based on salary?
 
-- Identify the top 5 in-demand skills for a data analyst.
-- Focus on all job postings.
-- Why? Retrieves the top 5 skills with the highest demand in the job market, providing insights into the most valuable skills for job seekers.
+- Look at the average salary associated with each skill for Data Analyst positions.
+- Focuses on roles with specified salaries, regardless of location.
+- Why? It reveals how different skills impact salary levels for Data Analysts and helps identify the most financially rewarding skills to acquire or improve.
 
 */
 
-
 SELECT 
     skills_dim.skills AS skills,
-    COUNT(skills_job_dim.job_id) AS demand_count -- count of the skills
+    ROUND(AVG(salary_year_avg),0) AS avg_salary -- average salary
 FROM 
     job_postings_fact
 INNER JOIN -- Inner join because we only want jobs that required skills
@@ -25,10 +23,11 @@ ON
     skills_job_dim.skill_id = skills_dim.skill_id
 WHERE 
     job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
     AND job_work_from_home = TRUE
 GROUP BY
     skills
 ORDER BY
-    demand_count DESC
+    avg_salary DESC
 LIMIT 5
 ;
